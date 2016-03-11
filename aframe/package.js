f26bb7c3ffe4a63e523bd52c74c1066e9ec77353
Vlad Balan <vlad.balan.dev@gmail.com>
2016-03-11 18:59:00 +0200

    Add version constraints

--- aframe/package.js
+++ aframe/package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   
-  api.use('vladbalan:aframe-blaze');
+  api.use('vladbalan:aframe-blaze@0.0.1');
 
   api.addFiles('client/src/aframe.js', 'client');
 });
